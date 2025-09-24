b4fa9a9e135761ad12cd427a5f7e60d24e98f47d
CatalystConnect <TPAUL@CATALYSTCONNECT.US>
2025-09-24 10:53:34 +0530

    work make crud for zip code

---
+++ migrations/20250924042356-create-taxes.js
@@ -0,0 +1,59 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('taxes', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      rate: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      zipcode: {
+        type: Sequelize.STRING(255),
+        allowNull: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('taxes');
+  }
+};

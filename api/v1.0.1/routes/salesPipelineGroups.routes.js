cbda1449b431f4269da083c40df8838b8748766d
CatalystConnect <TPAUL@CATALYSTCONNECT.US>
2025-09-25 18:08:29 +0530

    changes

--- api/v1.0.1/routes/salesPipelineGroups.routes.js
+++ api/v1.0.1/routes/salesPipelineGroups.routes.js
@@ -5,18 +5,18 @@ var { authJwt } = require("../middleware");
 
 
 /*addSalesPipelinesGroups*/
-// router.post("/sales-pipelines-groups",[authJwt.verifyToken],  controller.addSalesPipelinesGroups);
+router.post("/sales-pipelines-groups",[authJwt.verifyToken],  controller.addSalesPipelinesGroups);
 
 // // /*getAllSalesPipelinesGroups*/
-// router.get("/sales-pipelines-groups",[authJwt.verifyToken],  controller.getAllSalesPipelinesGroups);
+router.get("/sales-pipelines-groups",[authJwt.verifyToken],  controller.getAllSalesPipelinesGroups);
 
 // // /*getSalesPipelinesGroupsById*/
-// router.get("/getSalesPipelinesGroupsById",[authJwt.verifyToken], [controller.validate("getSalesPipelinesGroupsById")], controller.getSalesPipelinesGroupsById);
+router.get("/getSalesPipelinesGroupsById",[authJwt.verifyToken], controller.getSalesPipelinesGroupsById);
 
 // // /*updateSalesPipelinesGroups*/
-// router.put("/updateSalesPipelinesGroups",[authJwt.verifyToken], [controller.validate("getSalesPipelinesGroupsById")], controller.updateSalesPipelinesGroups);
+router.put("/updateSalesPipelinesGroups",[authJwt.verifyToken], controller.updateSalesPipelinesGroups);
 
 // /*deleteSalesPipelinesGroups*/
-// router.delete("/deleteSalesPipelinesGroups",[authJwt.verifyToken], [controller.validate("getSalesPipelinesGroupsById")], controller.deleteSalesPipelinesGroups);
+router.delete("/deleteSalesPipelinesGroups",[authJwt.verifyToken], controller.deleteSalesPipelinesGroups);
 
 module.exports = router;

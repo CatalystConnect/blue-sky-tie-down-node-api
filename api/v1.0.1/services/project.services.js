11f55d137b1b3eeba9d0b7a3be17ff2a3074b465
acewebx <CatalystConnect>
2025-10-01 14:32:13 +0530

    update api

--- api/v1.0.1/services/project.services.js
+++ api/v1.0.1/services/project.services.js
@@ -4,250 +4,285 @@ const db = require("../models");
 const { Op, fn, col, where } = require("sequelize");
 
 module.exports = {
-  /*addProject*/
-  async addProject(postData) {
-    try {
-      let addProject = await db.projectObj.create(postData);
-      return addProject;
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
-  /*getAllProject*/
-  async getAllProject(page = 1, length = 10, search = "") {
-    try {
-      const limit = parseInt(length) || 10;
-      const offset = (parseInt(page) - 1) * limit || 0;
+    /*addProject*/
+    async addProject(postData) {
+        try {
+            let addProject = await db.projectObj.create(postData);
+            return addProject;
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
+    /*getAllProject*/
+    async getAllProject(page = 1, length = 10, search = "") {
+        try {
+            const limit = parseInt(length) || 10;
+            const offset = (parseInt(page) - 1) * limit || 0;
 
-      const whereClause = {};
-      if (search) {
-        whereClause.name = { [db.Sequelize.Op.like]: `%${search}%` };
-      }
+            const whereClause = {};
+            if (search) {
+                whereClause.name = { [db.Sequelize.Op.like]: `%${search}%` };
+            }
 
-      const { rows, count } = await db.projectObj.findAndCountAll({
-        where: whereClause,
-        limit,
-        offset,
-        attributes: [
-          "id",
-          "user_id",
-          "site_plan_id",
-          "engineer_id",
-          "name",
-          "city",
-          "state",
-          "plan_date",
-          "bldg_gsqft",
-          "address",
-          "zip",
-          "units",
-          "projectType",
-          "project_phase",
-          "date_received",
-          "rev_status",
-          "plan_reviewed_date",
-          "plan_reviewed_by",
-          "plan_revision_notes",
-          "data_collocated_date",
-          "bldgs",
-          "wind_zone",
-          "seismic_zone",
-          "developer_id",
-          "general_contractor_id",
-          "assign_to_budget",
-          "take_off_team_id",
-          "take_off_type",
-          "take_off_scope",
-          "assign_date",
-          "plan_link",
-          "submissionType",
-          "planFiles",
-          "projectTags",
-          "projectFiles",
-          "architecture",
-          "takeoffactualtime",
-          "dueDate",
-          "projectAttachmentUrls",
-          "attachmentsLink",
-          "projectRifFields",
-        ],
-        include: [
-          { model: db.companyObj, as: "engineer" },
-          { model: db.companyObj, as: "architect" },
-          { model: db.companyObj, as: "developer" },
-          { model: db.companyObj, as: "general_contractor" },
-          { model: db.userObj, as: "planReviewer" },
-          { model: db.projectplanSetsObj, as: "planSets" },
-        ],
-        order: [["id", "DESC"]],
-      });
+            const { rows, count } = await db.projectObj.findAndCountAll({
+                where: whereClause,
+                limit,
+                offset,
+                attributes: [
+                    "id",
+                    "user_id",
+                    "site_plan_id",
+                    "engineer_id",
+                    "name",
+                    "city",
+                    "state",
+                    "plan_date",
+                    "bldg_gsqft",
+                    "address",
+                    "zip",
+                    "units",
+                    "projectType",
+                    "project_phase",
+                    "date_received",
+                    "rev_status",
+                    "plan_reviewed_date",
+                    "plan_reviewed_by",
+                    "plan_revision_notes",
+                    "data_collocated_date",
+                    "bldgs",
+                    "wind_zone",
+                    "seismic_zone",
+                    "developer_id",
+                    "general_contractor_id",
+                    "assign_to_budget",
+                    "take_off_team_id",
+                    "take_off_type",
+                    "take_off_scope",
+                    "assign_date",
+                    "plan_link",
+                    "submissionType",
+                    "planFiles",
+                    "projectTags",
+                    "projectFiles",
+                    "architecture",
+                    "takeoffactualtime",
+                    "dueDate",
+                    "projectAttachmentUrls",
+                    "attachmentsLink",
+                    "projectRifFields",
+                ],
+                include: [
+                    { model: db.companyObj, as: "engineer" },
+                    { model: db.companyObj, as: "architect" },
+                    { model: db.companyObj, as: "developer" },
+                    { model: db.companyObj, as: "general_contractor" },
+                    { model: db.userObj, as: "planReviewer" },
+                    { model: db.projectplanSetsObj, as: "planSets" },
+                ],
+                order: [["id", "DESC"]],
+            });
 
-      return {
-        data: rows,
-        meta: {
-          current_page: parseInt(page),
-          from: offset + 1,
-          to: offset + rows.length,
-          last_page: Math.ceil(count / limit),
-          per_page: limit,
-          total: count,
-        },
-      };
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
-  // /*getProjectById*/
-  async getProjectById(projectId) {
-    try {
-      let getProjectById = await db.projectObj.findOne({
-        where: { id: projectId },
-        attributes: [
-          "id",
-          "user_id",
-          "site_plan_id",
-          "engineer_id",
-          "name",
-          "city",
-          "state",
-          "plan_date",
-          "bldg_gsqft",
-          "address",
-          "zip",
-          "units",
-          "projectType",
-          "project_phase",
-          "date_received",
-          "rev_status",
-          "plan_reviewed_date",
-          "plan_reviewed_by",
-          "plan_revision_notes",
-          "data_collocated_date",
-          "bldgs",
-          "wind_zone",
-          "seismic_zone",
-          "developer_id",
-          "general_contractor_id",
-          "assign_to_budget",
-          "take_off_team_id",
-          "take_off_type",
-          "take_off_scope",
-          "assign_date",
-          "plan_link",
-          "submissionType",
-          "planFiles",
-          "projectTags",
-          "projectFiles",
-          "architecture",
-          "takeoffactualtime",
-          "dueDate",
-          "projectAttachmentUrls",
-          "attachmentsLink",
-          "projectRifFields",
-        ],
-        include: [
-          { model: db.companyObj, as: "engineer" },
-          { model: db.companyObj, as: "architect" },
-          { model: db.companyObj, as: "developer" },
-          { model: db.companyObj, as: "general_contractor" },
-          { model: db.userObj, as: "planReviewer" },
-          { model: db.projectplanSetsObj, as: "planSets" },
-          { model: db.leadsObj, as: "project_leads" },
-        ],
-      });
-      return getProjectById;
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
-  // /*updateProject*/
-  async updateProject(data, projectId) {
-    try {
-      let updateProject = await db.projectObj.update(data, {
-        where: { id: projectId },
-      });
-      return updateProject;
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
-  // /*deleteProject*/
-  async deleteProject(projectId) {
-    try {
-      let deleteProject = await db.projectObj.destroy({
-        where: { id: projectId },
-      });
-      return deleteProject;
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
-  async projectplanSets(postData) {
-    try {
-      let projectplanSets = await db.projectplanSetsObj.create(postData);
-      return projectplanSets;
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
+            return {
+                data: rows,
+                meta: {
+                    current_page: parseInt(page),
+                    from: offset + 1,
+                    to: offset + rows.length,
+                    last_page: Math.ceil(count / limit),
+                    per_page: limit,
+                    total: count,
+                },
+            };
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
+    // /*getProjectById*/
+    async getProjectById(projectId) {
+        try {
+            let getProjectById = await db.projectObj.findOne({
+                where: { id: projectId },
+                attributes: [
+                    "id",
+                    "user_id",
+                    "site_plan_id",
+                    "engineer_id",
+                    "name",
+                    "city",
+                    "state",
+                    "plan_date",
+                    "bldg_gsqft",
+                    "address",
+                    "zip",
+                    "units",
+                    "projectType",
+                    "project_phase",
+                    "date_received",
+                    "rev_status",
+                    "plan_reviewed_date",
+                    "plan_reviewed_by",
+                    "plan_revision_notes",
+                    "data_collocated_date",
+                    "bldgs",
+                    "wind_zone",
+                    "seismic_zone",
+                    "developer_id",
+                    "general_contractor_id",
+                    "assign_to_budget",
+                    "take_off_team_id",
+                    "take_off_type",
+                    "take_off_scope",
+                    "assign_date",
+                    "plan_link",
+                    "submissionType",
+                    "planFiles",
+                    "projectTags",
+                    "projectFiles",
+                    "architecture",
+                    "takeoffactualtime",
+                    "dueDate",
+                    "projectAttachmentUrls",
+                    "attachmentsLink",
+                    "projectRifFields",
+                ],
+                include: [
+                    { model: db.companyObj, as: "engineer" },
+                    { model: db.companyObj, as: "architect" },
+                    { model: db.companyObj, as: "developer" },
+                    { model: db.companyObj, as: "general_contractor" },
+                    { model: db.userObj, as: "planReviewer" },
+                    { model: db.projectplanSetsObj, as: "planSets" },
+                    { model: db.leadsObj, as: "project_leads" },
+                ],
+            });
+            return getProjectById;
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
+    // /*updateProject*/
+    async updateProject(data, projectId) {
+        try {
+            let updateProject = await db.projectObj.update(data, {
+                where: { id: projectId },
+            });
+            return updateProject;
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
+    // /*deleteProject*/
+    async deleteProject(projectId) {
+        try {
+            let deleteProject = await db.projectObj.destroy({
+                where: { id: projectId },
+            });
+            return deleteProject;
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
+    async projectplanSets(postData) {
+        try {
+            let projectplanSets = await db.projectplanSetsObj.create(postData);
+            return projectplanSets;
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
 
-  /*addProjectNotes*/
-  async addProjectNotes(postData) {
-    try {
-      const addProjectNotes = await db.projectNotesObj.create(postData);
-      return addProjectNotes;
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
+    /*addProjectNotes*/
+    async addProjectNotes(postData) {
+        try {
+            const addProjectNotes = await db.projectNotesObj.create(postData);
+            return addProjectNotes;
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
 
-  async listProjectNotes(projectId, limit) {
-    try {
-      return await db.projectNotesObj.findAll({
-        where: { project_id: projectId },
-        order: [["created_at", "DESC"]],
-        limit: limit,
-      });
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
+    async listProjectNotes(projectId, limit) {
+        try {
+            return await db.projectNotesObj.findAll({
+                where: { project_id: projectId },
+                order: [["created_at", "DESC"]],
+                limit: limit,
+            });
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
 
-  // Update Project Note
-  async updateProjectNotes(projectNoteId, notes) {
-    try {
-      const [updated] = await db.projectNotesObj.update(
-        { notes: notes },
-        { where: { id: projectNoteId } }
-      );
+    // Update Project Note
+    async updateProjectNotes(projectNoteId, notes) {
+        try {
+            const [updated] = await db.projectNotesObj.update(
+                { notes: notes },
+                { where: { id: projectNoteId } }
+            );
 
-      if (updated === 0) return null;
+            if (updated === 0) return null;
 
-      return await db.projectNotesObj.findByPk(projectNoteId);
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
+            return await db.projectNotesObj.findByPk(projectNoteId);
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
 
-  // Delete Project Notes
-  async deleteProjectNotes(projectNoteId) {
-    try {
-      const deleted = await db.projectNotesObj.destroy({
-        where: { id: projectNoteId },
-      });
-      return deleted > 0;
-    } catch (e) {
-      logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
-      throw e;
-    }
-  },
+    // Delete Project Notes
+    async deleteProjectNotes(projectNoteId) {
+        try {
+            const deleted = await db.projectNotesObj.destroy({
+                where: { id: projectNoteId },
+            });
+            return deleted > 0;
+        } catch (e) {
+            logger.errorLog.log("error", commonHelper.customizeCatchMsg(e));
+            throw e;
+        }
+    },
+    async getProjectPlanSet(filters) {
+        try {
+            const whereClause = { project_id: filters.project_id };
+
+            if (filters.id) {
+                whereClause.id = filters.id;
+            }
+
+            if (filters.search) {
+                whereClause[Op.or] = [
+                    { plan_link: { [Op.like]: `%${filters.search}%` } },
+                    { submissionType: { [Op.like]: `%${filters.search}%` } },
+                    { plan_revision_notes: { [Op.like]: `%${filters.search}%` } }
+                ];
+            }
+
+            const planSets = await db.projectplanSetsObj.findAll({
+                where: whereClause,
+                order: [["id", "DESC"]],
+                include: [
+                    {
+                        model: db.userObj,
+                        as: "reviewedByUser",
+                        attributes: ["id", "name", "email"]
+                    }
+                ]
+            });
+
+            return planSets;
+        } catch (e) {
+            console.error("Error in getProjectPlanSet:", e);
+            throw e;
+        }
+
+    },
 };

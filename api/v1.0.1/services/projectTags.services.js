74222a6b35d6afc4d95a5f383ec139ef10f78fd4
acewebx <CatalystConnect>
2025-10-29 10:02:38 +0530

    update state order

--- api/v1.0.1/services/projectTags.services.js
+++ api/v1.0.1/services/projectTags.services.js
@@ -32,7 +32,7 @@ module.exports = {
 
       const { rows, count } = await db.projectTagsObj.findAndCountAll({
         where: whereCondition,
-        order: [["id", "DESC"]],
+        order: [["order", "ASC"]],
         limit,
         offset,
       });

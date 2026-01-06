62bec75b66e92760ff7b242d86f61ab80322ea58
acewebx <CatalystConnect>
2026-01-06 12:24:08 +0530

    update vendor

--- api/v1.0.1/controllers/vendor.controller.js
+++ api/v1.0.1/controllers/vendor.controller.js
@@ -21,18 +21,17 @@ module.exports = {
           .status(400)
           .send(commonHelper.parseErrorRespose(errors.mapped()));
       }
-
+      
       const { user_id, name, email, phone, bill_to, ship_to, vendor_code, dba_name, tax_id, is_1099_eligible, payment_terms_id, incoterm_code, freight_terms, default_shipping_method_id, status } = req.body;
 
-      if (!Object.values(VENDOR_FREIGHT_TERMS).includes(freight_terms)) {
+      if (freight_terms && !Object.values(VENDOR_FREIGHT_TERMS).includes(freight_terms)) {
         return res.status(400).send({
           status: false,
-          message: `Invalid freight_terms. Allowed: ${Object.values(
-            VENDOR_FREIGHT_TERMS
-          ).join(", ")}`,
+          message: `Invalid freight_terms. Allowed: ${Object.values(VENDOR_FREIGHT_TERMS).join(", ")}`,
           data: {},
         });
       }
+
 
 
       if (!Object.values(VENDOR_STATUS).includes(status)) {
@@ -45,22 +44,38 @@ module.exports = {
         });
       }
       const postData = {
+        // user_id: req.userId,
+        // name,
+        // email,
+        // phone,
+        // bill_to,
+        // ship_to,
+        // vendor_code,
+        // dba_name,
+        // tax_id,
+        // is_1099_eligible,
+        // payment_terms_id,
+        // incoterm_code,
+        // freight_terms,
+        // default_shipping_method_id,
+        // status,
         user_id: req.userId,
         name,
+        vendor_code,
         email,
         phone,
         bill_to,
         ship_to,
-        vendor_code,
-        dba_name,
         tax_id,
-        is_1099_eligible,
-        payment_terms_id,
-        incoterm_code,
-        freight_terms,
         default_shipping_method_id,
         status,
+        dba_name: req.body.dba_name || null,
+        is_1099_eligible: req.body.is_1099_eligible || false,
+        payment_terms_id: req.body.payment_terms_id || null,
+        incoterm_code: req.body.incoterm_code || null,
+        freight_terms: req.body.freight_terms || "HARD"
       };
+      
 
       const vendor = await vendorServices.addVendors(postData);
 
